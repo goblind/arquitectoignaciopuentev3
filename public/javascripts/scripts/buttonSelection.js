5ad895e77485f27d9e8cd8542f3e1c65ce22ae7c
goblind <shields.daniel@gmail.com>
2014-10-08 19:57:06 -0300

    admin module

--- public/javascripts/scripts/buttonSelection.js
+++ public/javascripts/scripts/buttonSelection.js
@@ -1,7 +1,20 @@
-$(function() {
-	$('#editarProyectoBotton', '#crearProyectoBotton').on('click', function(){		
-		($(this)).siblings().toggleClass();
-		($(this)).addClass('active');
+$(function() {	
+	$('#editarProyecto').hide();
+	$('#crearProyecto').hide();
+
+	$('#editarProyectoBoton').on('click', function(){
+		$('#crearProyecto').hide();			
+		$('#editarProyecto').show();
+	});
+
+	$('#crearProyectoBoton').on('click', function(){
+		$('#editarProyecto').hide();		
+		$('#crearProyecto').show();
 	});	
+
+	$("#editarProyectoLista li a").click(function(){
+      		$("#editarProyectoTitulo").text($(this).text());
+      		
+   	});
 });
 
