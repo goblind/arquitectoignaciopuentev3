19693be77a1ae9e8a09119cfba935a89b0f8eab9
goblind <shields.daniel@gmail.com>
2014-10-14 20:51:42 -0300

    more os

--- config/database/connection.js
+++ config/database/connection.js
@@ -5,16 +5,31 @@ var mysql = require('mysql');
 
 //pool de conexión
 var pool = module.exports = mysql.createPool({  
-    //host: 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/',    
-    //host: 'mysql://127.10.225.2:3306/',
-    host: 'mysql://adminJSbgVLs:gfZgnRl9ND7G@127.10.225.2:3306/',
-    user: 'adminJSbgVLs',
-    password: 'gfZgnRl9ND7G',
-    database: 'arquitectoignaciopuente',
+    // //host: 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/',    
+    // host: 'mysql://127.10.225.2:3306/',
+    // //host: 'mysql://adminJSbgVLs:gfZgnRl9ND7G@127.10.225.2:3306/',
+    // user: 'adminJSbgVLs',
+    // password: 'gfZgnRl9ND7G',
+    // database: 'arquitectoignaciopuente',
+
+	host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+  	user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+  	pass     : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+  	port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+  	database : 'arquitectoignaciopuente'    
     connectionLimit: 400,
     multipleStatements: true,
     supportBigNumbers: true
 });
+/*
+ var connection = mysql.createConnection({
+  host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+  user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+  pass     : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+  port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+  database : 'stembuds'
+ });
+*/
 
 /*
 usuario admin:
